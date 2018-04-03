616a188adebcfb3218e1b6fbc4bb184ff3b7338a
valery <valery.fremaux@gmail.com>
2018-04-03 15:47:48 +0200

    first code load

---
+++ lang/fr/block_quiz_behaviour.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Block Quiz Behaviour language strings.
+ *
+ * @package    block_quiz_behaviour
+ * @copyright  2018 Valery Fremaux
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['quiz_behaviour:addinstance'] = 'Ajouter le bloc Quiz Behaviour';
+
+$string['afterattempt'] = 'Après la tentative';
+$string['alternateattemptpage'] = 'Disposition alternative de la page de test';
+$string['deadend'] = 'Cul de sac';
+$string['deadendmessage'] = 'Message de cul de sac';
+$string['deadendpage'] = 'Fin du test';
+$string['directreturn'] = 'Retour direct';
+$string['duringattempt'] = 'Pendant la tentative';
+$string['forceanswer'] = 'Forcer la réponse';
+$string['help'] = 'Editez les paramètres de ce bloc pour ajuster les comportements des tests.';
+$string['hideflags'] = 'Cacher le marquage des questions';
+$string['hidesummaryinfo'] = 'Cacher les informations du test dans le sommaire';
+$string['looseattemptsignal'] = 'Vous quittez le quiz avec pour effet de perdre la tentative en cours. Voulez bvous continuer ?';
+$string['nobackwards'] = 'Pas de retour en arrère';
+$string['pluginname'] = 'Comportement de quiz';
+$string['quizbehaviourconfig'] = 'Configuration du comportement des tests';
+$string['screenprotect'] = 'Protection de contenu';
+$string['trapoutlinks'] = 'Piéger les liens sortants';
+$string['youhave'] = 'Vous avez {$a} tests dans ce cours.';
