cfa73c39d141a6c033b9b1301aedac83db996a03
valery <valery.fremaux@gmail.com>
2018-04-03 15:46:31 +0200

    first code load

--- README.md
+++
@@ -1,2 +0,0 @@
-# moodle-block_quiz_behaviour
-Add special behaviour to quiz that derrogate to standard quiz cinematic
---
+++ lang/en/block_quiz_behaviour.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Block Quiz Behaviour language strings.
+ *
+ * @package    block_quiz_behaviour
+ * @copyright  2018 Valery Fremaux
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['quiz_behaviour:addinstance'] = 'Add a Quiz Behaviour block';
+
+$string['afterattempt'] = 'After attempt';
+$string['alternateattemptpage'] = 'Alternative layout of the attempt page';
+$string['deadend'] = 'Dead end';
+$string['deadendmessage'] = 'Dead end message';
+$string['directreturn'] = 'Direct return';
+$string['deadendpage'] = 'Quiz exit';
+$string['duringattempt'] = 'During attempt';
+$string['forceanswer'] = 'Force answer';
+$string['help'] = 'Just edit this block to change behaviour of quizzes.';
+$string['hideflags'] = 'Hide question flagging';
+$string['hidesummaryinfo'] = 'Hide summary info';
+$string['looseattemptsignal'] = 'You are quitting the test and will loose all results of the attempt. Do you want to continue?';
+$string['nobackwards'] = 'No backwards';
+$string['pluginname'] = 'Quiz Behaviour Modifier';
+$string['quizbehaviourconfig'] = 'Quiz behaviour configuration';
+$string['screenprotect'] = 'Screen protect';
+$string['trapoutlinks'] = 'Trap outgoing links';
+$string['youhave'] = 'You have {$a} quizzes in this course';
